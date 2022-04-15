5bf159d91aa4de44a805285968260ae1c4d0610f
ShcherbinaNick <scherbinanick@yandex.ru>
2022-04-16 00:31:39 +0300

    crash-test added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,12 +8,22 @@ const NotFoundError = require('../errors/NotFoundError');
 
 const routes = express.Router();
 
+routes.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 routes.post('/signup', express.json(), validateCreateUser, createUser);
 routes.post('/signin', express.json(), validateLogin, login);
 
 routes.use('/users', auth, userRoutes);
 routes.use('/cards', auth, cardsRoutes);
 routes.use('/logout', logout);
+
+// Логаут по заданию не требуется, но мне захотелось его сделать.
+// Локально работал, а удалённо не хочет, уже очень долго бьюсь.
+// Буду очень рад, если укажете на проблему
 
 routes.use((req, res, next) => {
   next(new NotFoundError('Cтраница не найдена'));
